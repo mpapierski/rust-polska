e4deb6954e792c5983170c3e0ef31be4f9e7d461
Michał Papierski <michal@papierski.net>
2019-01-28 22:47:42 +0100

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+extern crate hyper;
+
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+use hyper::{Body, Response, Server};
+
+static TEXT: &str = "Hello, Rust Polska!";
+
+fn main() {
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let new_svc = || service_fn_ok(|_req| Response::new(Body::from(TEXT)));
+
+    let server = Server::bind(&addr)
+        .serve(new_svc)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    hyper::rt::run(server);
+}
