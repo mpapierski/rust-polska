470d2a166702e8fe487547dad4dc605e08026b93
Michał Papierski <michal@papierski.net>
2019-01-28 23:50:12 +0100

    Use $PORT.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 extern crate hyper;
 
+use std::env;
+
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
 use hyper::{Body, Response, Server};
 
 static TEXT: &str = "Hello, Rust Polska!";
 
-fn main() {
-    let addr = ([127, 0, 0, 1], 3000).into();
+fn main() -> Result<(), Box<std::error::Error>> {
+    // Heroku passes $PORT
+    let port = env::var("PORT")
+        .ok()
+        .and_then(|v| v.parse().ok())
+        // For local development port 3000 is used if no $PORT is found.
+        .unwrap_or(3000);
+    let addr = ([127, 0, 0, 1], port).into();
 
     let new_svc = || service_fn_ok(|_req| Response::new(Body::from(TEXT)));
 
@@ -16,4 +24,6 @@ fn main() {
         .map_err(|e| eprintln!("server error: {}", e));
 
     hyper::rt::run(server);
+
+    Ok(())
 }
